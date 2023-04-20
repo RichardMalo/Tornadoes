ff5ee6ab5a003360fce609600bafb7633fae6289
Richard Malolepszy <86073298+Rolosaregood@users.noreply.github.com>
2023-04-20 14:29:57 -0400

    updated app.py, index.html, sqlitefile, makemap.js

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def load_csv_data():
     df = pd.read_csv('data/1950-2021_torn.csv')
     # Add this line to select specific columns
     # yr=year, mag=magnitude, inj=injuries, fat =fatalities, slon=starting longitude, slat =starting latiude, st=state, len=length of tornado, wid=width of tornado.
-    selected_columns = ['yr', 'mag', 'inj', 'fat', 'slon', 'slat', 'st', 'len', 'wid']  
+    selected_columns = ['yr', 'mag', 'inj', 'fat', 'slon', 'slat'] #, 'st', 'len', 'wid']  
     # Update this line to return only the selected columns
     return df[selected_columns].to_json(orient='records')  
 
