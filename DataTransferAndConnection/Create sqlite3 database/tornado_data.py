eef381f293c2469e0e4880c3b90eb3a657827986
Gavin <gavinwan0929@gmail.com>
2023-04-23 10:07:46 -0400

    upload data file

--- DataTransferAndConnection/Create sqlite3 database/tornado_data.py
+++ DataTransferAndConnection/Create sqlite3 database/tornado_data.py
@@ -6,7 +6,7 @@ conn = sqlite3.connect('tornado10ColP3.sqlite')
 
 # create a new table
 cursor = conn.cursor()
-cursor.execute('''CREATE TABLE tornado_table2 (yr INTEGER, mo INTEGER, st INTEGER, mag INTEGER,inj INTEGER, fat INTEGER, slat INTEGER, slon INTEGER, len INTEGER, wid INTEGER);''')
+cursor.execute('''CREATE TABLE tornado_table2 (yr INTEGER, mo INTEGER, st TEXT, mag INTEGER, inj INTEGER, fat INTEGER, slat INTEGER, slon INTEGER, len INTEGER, wid INTEGER);''')
 
 # import data from CSV file
 with open('1950-2021_torn.csv', 'r') as file:
@@ -18,7 +18,7 @@ with open('1950-2021_torn.csv', 'r') as file:
         # extract the values from the row and convert to the appropriate data types
         yr = int(row[1])
         mo = int(row[2])
-        st = NameError(row[7])
+        st = row[6]
         mag = int(row[10])
         inj = int(row[11])
         fat = int(row[12])
@@ -28,17 +28,8 @@ with open('1950-2021_torn.csv', 'r') as file:
         wid = int(row[20])
 
         # insert the values into the table
-        cursor.execute('''INSERT INTO tornado_tables (yr, mo, st, mag, inj, fat, slat, slon, len, wid) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''', (yr, mo, st, mag, inj, fat, slat, slon, len, wid))
+        cursor.execute('''INSERT INTO tornado_table2 (yr, mo, st, mag, inj, fat, slat, slon, len, wid) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);''', (yr, mo, st, mag, inj, fat, slat, slon, len, wid))
 
 # commit changes and close the connection
 conn.commit()
 conn.close()
-
-
-
-
-
-
-
-
-
